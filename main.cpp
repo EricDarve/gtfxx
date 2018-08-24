5e49c5580c7605aca84429a7b53369feb5e801db
Eric Darve <darve@stanford.edu>
2018-08-24 11:22:22 -0700

    Updated code for better profiling

--- main.cpp
+++ main.cpp
@@ -81,59 +81,123 @@ LogMessage(__FILE__,__func__,__LINE__) << out; \
 #endif
 
 struct Profiling_Event {
-    std::string thread_id;
-    std::string name;
-    std::chrono::high_resolution_clock::time_point m_start, m_end;
+    enum event {duration,timepoint};
 
-    Profiling_Event()
+    std::string name;
+    std::string thread_id;
+    std::chrono::high_resolution_clock::time_point m_start, m_stop;
+    event ev_type;
+
+    Profiling_Event(char* c, std::string& t_id) : name(c), thread_id(t_id)
     {
-        LOG("Default Profiling_Event constructor called");
     }
 
-    Profiling_Event(char * c) : name(c)
+    void timestamp()
     {
-        std::ostringstream string_id;
-        string_id << std::this_thread::get_id();
-        thread_id = string_id.str();
+        ev_type = timepoint;
         m_start = std::chrono::high_resolution_clock::now();
     }
 
-    void end()
+    void start()
     {
-        m_end = std::chrono::high_resolution_clock::now();
+        ev_type = duration;
+        m_start = std::chrono::high_resolution_clock::now();
+    }
+
+    void stop()
+    {
+        m_stop = std::chrono::high_resolution_clock::now();
     }
 };
+
+struct Thread_team;
+
+std::string id_to_string(std::thread::id id)
+{
+    std::stringstream s_id;
+    s_id << id;
+    return s_id.str();
+}
+
+std::string get_thread_id()
+{
+    return id_to_string( std::this_thread::get_id() );
+}
 
 struct Profiler {
     list<Profiling_Event> events;
     std::mutex mtx;
+    std::map<std::string, int> thread_id_map;
+    std::string file_name{"prof.out"};
 
-    void push(Profiling_Event & a_pe)
+    void map_team_threads(Thread_team &);
+    
+    void open(char * c) {
+        // Open a new file and clear up the list of events
+        file_name = std::string(c);
+        events.clear();
+    }
+
+    void timestamp(char* c)
     {
-        std::lock_guard<std::mutex> lck(mtx);
-        events.push_back(a_pe);
+        auto thread_id = get_thread_id();
+        Profiling_Event prof_ev(c, thread_id);
+        prof_ev.timestamp();
+        {
+            std::lock_guard<std::mutex> lck(mtx);
+            events.push_back(prof_ev);
+        }
+    }
+
+    Profiling_Event* start(char *c)
+    {
+        auto thread_id = get_thread_id();
+        Profiling_Event* prof_ev = new Profiling_Event(c, thread_id);
+        prof_ev->start();
+        return prof_ev;
+    }
+
+    void stop(Profiling_Event* prof_ev)
+    {
+        prof_ev->stop();
+        {
+            std::lock_guard<std::mutex> lck(mtx);
+            events.push_back(*prof_ev);
+        }
+        delete prof_ev;
     }
 
     void dump()
     {
-        FILE* fp = std::fopen("prof.out", "w");
+        FILE* fp = std::fopen(file_name.c_str(), "w");
         if(!fp) {
             std::perror("File opening failed");
             return;
         }
 
-        auto it = events.begin();
+        fprintf(fp,"nthreads %d\n",thread_id_map.size());
+        for (auto it = thread_id_map.begin(); it != thread_id_map.end(); ++it) {
+            fprintf(fp,"tidmap %s %d\n",it->first.c_str(),it->second);
+        }
 
-        for (; it != events.end(); ++it) {
+        for (auto it = events.begin(); it != events.end(); ++it) {
             auto d_start = std::chrono::duration<long long, std::nano>(it->m_start.time_since_epoch());
-            auto d_end   = std::chrono::duration<long long, std::nano>(it->m_end.time_since_epoch());
-            fprintf(fp,"tid: %s start: %ld end: %ld name: %s\n",it->thread_id.c_str(),
-                    d_start.count(),d_end.count(),it->name.c_str());
+            switch (it->ev_type) {
+            case Profiling_Event::duration :
+                auto d_end   = std::chrono::duration<long long, std::nano>(it->m_stop.time_since_epoch());
+                fprintf(fp,"tid %s start %ld end %ld name %s\n",it->thread_id.c_str(),
+                        d_start.count(),d_end.count(),it->name.c_str());
+                break;
+            case Profiling_Event::timepoint :
+                fprintf(fp,"tid %s timestamp %ld name %s\n",it->thread_id.c_str(),
+                        d_start.count(),it->name.c_str());
+                break;
+            }
         }
 
         std::fclose(fp);
     }
-} prof;
+} profiler;
 
 struct Task : public Base_task {
     float m_priority = 0.0; // task priority
@@ -175,7 +239,6 @@ public:
 
 struct Thread_prio;
 void spin(Thread_prio * a_thread);
-struct Thread_team;
 
 // Thread with priority queue management
 struct Thread_prio {
@@ -284,12 +347,19 @@ struct Thread_team : public vector<Thread_prio*> {
             for (unsigned long i=a_id+1; i<a_id+n_query; ++i) {
                 auto j = i%v_thread.size();
                 if (v_thread[j].m_empty.load()) {
-                    //LOG("Task spawned on alien thread");
                     id_ = static_cast<int>(j);
                     break;
                 }
             }
         }
+        char timestamp_message[80];
+        if (id_ == a_id) {
+            sprintf(timestamp_message,"spawn_self %d",a_id);
+        }
+        else {
+            sprintf(timestamp_message,"spawn_other %d to %d",a_id,id_);
+        }
+        profiler.timestamp(timestamp_message);
         // Note that because we are not using locks, the queue may no longer be empty.
         // But this implementation is more efficient than using locks.
         ++ntasks;
@@ -305,7 +375,7 @@ struct Thread_team : public vector<Thread_prio*> {
             if (!thread_.m_empty.load()) {
                 std::unique_lock<std::mutex> lck(thread_.mtx);
                 if (!thread_.ready_queue.empty()) {
-                    //LOG("Task stolen");
+                    profiler.timestamp("steal");
                     Task * tsk = thread_.pop();
                     lck.unlock();
                     v_thread[a_id].spawn(tsk);
@@ -319,12 +389,15 @@ struct Thread_team : public vector<Thread_prio*> {
 // Keep executing tasks until m_stop = true && there are no tasks left anywhere
 void spin(Thread_prio * a_thread)
 {
+    auto pe = profiler.start("overhead");
     std::unique_lock<std::mutex> lck(a_thread->mtx);
     while (true) {
         while (!a_thread->ready_queue.empty()) {
             Task * tsk = a_thread->pop();
             lck.unlock();
+            profiler.stop(pe);
             (*tsk)();
+            pe = profiler.start("overhead");
             --(a_thread->team->ntasks);
             if (tsk->m_delete) delete tsk;
             lck.lock();
@@ -339,17 +412,33 @@ void spin(Thread_prio * a_thread)
             // Return if stop=true and no tasks are left
             if (a_thread->team->m_stop.load() && a_thread->team->ntasks.load() <= 0) {
                 lck.unlock();
+                profiler.stop(pe);
                 // Wake up all threads and exits
                 a_thread->team->wake_all();
                 return;
             }
             // Wait if queue is empty
-            Profiling_Event pe("wait");
+            profiler.stop(pe);
+            pe = profiler.start("wait");
+            
             a_thread->cv.wait(lck);
-            pe.end();
-            prof.push(pe);
+            
+//            lck.unlock();
+//            std::this_thread::yield();
+//            lck.lock();
+            
+            profiler.stop(pe);
+            pe = profiler.start("overhead");
         }
     }
+}
+
+void Profiler::map_team_threads(Thread_team & team)
+{
+    for (auto& th_prio : team.v_thread) {
+        thread_id_map[ id_to_string(th_prio.th.get_id()) ] = th_prio.m_id;
+    }
+    thread_id_map[id_to_string(std::this_thread::get_id()) ] = -1;
 }
 
 struct Matrix3_task : vector<Task> {
@@ -638,17 +727,15 @@ void pass_the_bucket(vector<Task>* tsk, vector<std::atomic_int>* counter,
                      Thread_team* team,
                      const int size, const int n_thread, int th)
 {
-    Profiling_Event pe0("task");
+    auto pe = profiler.start("task");
     ++(*counter)[th%size];
     std::this_thread::sleep_for(microseconds(10));
-    pe0.end();
-    prof.push(pe0);
-    Profiling_Event pe1("spawn");
+    profiler.stop(pe);
+    pe = profiler.start("spawn");
     if (th%size!= size-1) {
         team->spawn((th+1)%n_thread, &(*tsk)[th+1]);
     }
-    pe1.end();
-    prof.push(pe1);
+    profiler.stop(pe);
 }
 
 TEST(TaskFlow, Ring)
@@ -691,7 +778,7 @@ TEST(TaskFlow, Ring)
         ASSERT_EQ(counter[i].load(), max_count*(i+1));
     }
 
-    prof.dump();
+    profiler.dump();
 }
 
 void test_ring_flow(const int n_thread,const int ring_size, const int max_count, const int stride)
@@ -874,9 +961,9 @@ TEST(TaskFlow, Eigen)
 TEST(TaskFlow, GEMM)
 {
     const int nb = 4; // number of blocks
-    const int b = 64;  // size of blocks
+    const int b = 32;  // size of blocks
 
-    const int n_thread = 8; // number of threads to use
+    const int n_thread = 2; // number of threads to use
 
     const int n = b*nb; // matrix size
 
@@ -898,7 +985,7 @@ TEST(TaskFlow, GEMM)
 
     // Calculation using task flow
     Block_matrix Ab(nb,nb), Bb(nb,nb), Cb(nb,nb);
-    
+
     for (int i=0; i<nb; ++i) {
         for (int j=0; j<nb; ++j) {
             Ab(i,j) = new MatrixXd(A.block(i*b,j*b,b,b));
@@ -912,7 +999,7 @@ TEST(TaskFlow, GEMM)
 
     // GEMM task flow
     Task_flow gemm_g(&team,nb,nb,nb);
-    
+
     auto compute_on_ij = [=] (int3& idx) {
         return ( ( idx[0] + nb * idx[1] ) % n_thread );
     };
@@ -926,20 +1013,25 @@ TEST(TaskFlow, GEMM)
 
         a_tsk->set_function([=,&gemm_g,&Ab,&Bb,&Cb] ()
         {
-            Profiling_Event pe("gemm");
+            auto pe = profiler.start("gemm");
             *(Cb(i,j)) += *(Ab(i,k)) * *(Bb(k,j)); // GEMM
+            profiler.stop(pe);
+            pe = profiler.start("dependency");
             if (k < nb - 1) {
                 gemm_g.decrement_wait_count({i,j,k+1});
             }
-            pe.end();
-            prof.push(pe);
+            profiler.stop(pe);
         });
         return (/*k*/ idx[2] == 0 ? 0 : 1); // wait_count
     })
     .compute_on(compute_on_ij);
+    
+    profiler.open("gemm_v2.out");
 
     // Start team of threads
     team.start();
+
+    profiler.map_team_threads(team);
 
     auto start = high_resolution_clock::now();
 
@@ -972,7 +1064,7 @@ TEST(TaskFlow, GEMM)
         }
     }
 
-    prof.dump();
+    profiler.dump();
 }
 
 TEST(TaskFlow, GEMMInit)
@@ -1038,7 +1130,7 @@ TEST(TaskFlow, GEMMInit)
         int j = idx[1];
         a_tsk->set_function([=,&ctx,&Ab,&Bb,&Cb] ()
         {
-            Profiling_Event pe("init mat");
+            auto pe = profiler.start("init mat");
             Ab(i,j) = new MatrixXd(A.block(i*b,j*b,b,b));
             Bb(i,j) = new MatrixXd(B.block(i*b,j*b,b,b));
             Cb(i,j) = new MatrixXd(MatrixXd::Zero(b,b));
@@ -1046,8 +1138,7 @@ TEST(TaskFlow, GEMMInit)
                 ctx.gemm_g.decrement_wait_count({i,k,0});
                 ctx.gemm_g.decrement_wait_count({k,j,0});
             }
-            pe.end();
-            prof.push(pe);
+            profiler.stop(pe);
         });
         return 0; // wait_count
     })
@@ -1062,13 +1153,12 @@ TEST(TaskFlow, GEMMInit)
 
         a_tsk->set_function([=,&ctx,&Ab,&Bb,&Cb] ()
         {
-            Profiling_Event pe("gemm");
+            auto pe = profiler.start("gemm");
             *(Cb(i,j)) += *(Ab(i,k)) * *(Bb(k,j)); // GEMM
             if (k < nb - 1) {
                 ctx.gemm_g.decrement_wait_count({i,j,k+1});
             }
-            pe.end();
-            prof.push(pe);
+            profiler.stop(pe);
         });
         return (/*k*/ idx[2] == 0 ? 2*nb : 1); // wait_count
     })
@@ -1113,7 +1203,7 @@ TEST(TaskFlow, GEMMInit)
         }
     }
 
-    prof.dump();
+    profiler.dump();
 }
 
 int main(int argc, char **argv)
